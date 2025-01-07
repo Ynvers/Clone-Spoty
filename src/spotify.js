2e3849a503db6470bb72214971f68de537b2d975
Ynvers <luzolo.adoho@gmail.com>
2025-01-07 16:51:35 +0100

    miise à jour de l'authentification

--- src/spotify.js
+++ src/spotify.js
@@ -1,2 +1,26 @@
-export const authEndpoint = "https://acount.spotify.com/authorize";
-const redirectUri = "https://localhost:3000/"
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:8000/"
+const clientId = "be994e83b6e64f45b1f0ebd19d47fcd1";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const getTokenFromUrl = () => {
+    return window.location.hash
+      .substring(1)
+      .split("&")
+      .reduce((initial, item) => {
+        let parts = item.split("=");
+        initial[parts[0]] = decodeURIComponent(parts[1]);
+        return initial;
+      }, {});
+  };
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+    "%20"
+)}&response_type=token&show_dialog=true`;
